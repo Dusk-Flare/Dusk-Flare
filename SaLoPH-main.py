33c9e3f675a68e8f74fb5a082dfa3daf195c2c85
DuskFlare13 <Redstoner113@gmail.com>
2024-09-16 00:24:14 -0300

    initial steps

---
+++ SaLoPH-main.py
@@ -0,0 +1,15 @@
+from SaFi import *
+
+valueA = 5
+valueB = 6
+valueC = 1
+
+encode = open("SaFi.py", "w")
+encode.write("valueA = " + str(valueA))
+encode.write("valueB = " + str(valueB))
+encode.write("valueC = " + str(valueC))
+encode.close()
+
+
+
+
